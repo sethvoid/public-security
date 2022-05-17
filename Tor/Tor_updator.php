ae4a6fab5b00300928555291170af33f932a80bf
Seth Wallace <sebastian.wallace.uk@gmail.com>
2022-05-17 11:43:54 +0100

    Update Tor_updator.php

--- Tor/Tor_updator.php
+++ Tor/Tor_updator.php
@@ -4,9 +4,10 @@ if (isset($argv[1]) && $argv[1] == '-help') {
     exit('https://metrics.torproject.org/collector/recent/exit-lists/' . PHP_EOL);
 }
 
-$fileName = input("Enter File name :");
-$db = input("Enter Database name :");
-$createStatement = input("Create table? Y/n :");
+$fileName = input("Enter download date [https://metrics.torproject.org/collector/recent/exit-lists/] :");
+$db = input("Enter Database name (optional) :");
+$createStatement = strtoupper(input("Create table? y/n :")) == 'Y' ? true : false;
+$includeCleanup = strtoupper(input("Include cleanup statement? y/n :")) == 'Y' ? true : false;
 $text = file_get_contents('https://collector.torproject.org/recent/exit-lists/' . $fileName);
 
 $exitNode = '';
@@ -36,8 +37,8 @@ if ($db != '') {
     $sql .= 'USE ' . $db . ';' . PHP_EOL;
 }
 
-if ($createStatement == 'Y' || $createStatement == 'y') {
-    $sql = 'CREATE TABLE IF NOT EXISTS `exit_nodes` (
+if ($createStatement) {
+    $sql .= 'CREATE TABLE IF NOT EXISTS `exit_nodes` (
       `ip` varchar(100) NOT NULL,
       `last_checked` varchar(45) DEFAULT NULL,
       `node_id` varchar(150) DEFAULT NULL,
@@ -53,7 +54,11 @@ foreach ($nodes as $key => $value) {
     $sql .= 'ON DUPLICATE KEY UPDATE last_checked = "' . $value['last_checked'] . '", node_id="' . $key . '";' . PHP_EOL;
 }
 
-file_put_contents('tor_update_script.sql',$sql);
+if ($includeCleanup) {
+    $sql .= 'UPDATE exit_nodes SET node_id="NOT_IN_USE" WHERE (last_checked != "' . $value['last_checked'] . '" OR last_checked IS NULL) 
+    AND node_id != "NOT_IN_USE";';
+    file_put_contents('tor_update_script.sql', $sql);
+}
 
 function input(string $prompt = null): string
 {
